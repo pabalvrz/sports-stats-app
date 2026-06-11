9dc098cacccc027d5fb8f4997e8bafd8a2899f7f
pabalvrz <pabalvrz97@gmail.com>
2026-06-11 22:35:10 +0200

    feat: initialize sports catalog service

---
+++ services/sports-catalog-service/src/main/java/com/pabalvrz/sportsstatsapp/SportsCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pabalvrz.sportsstatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsCatalogServiceApplication.class, args);
+	}
+
+}
